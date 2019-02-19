bf9553310d3e727146478b5505e6c6a30e43eee9
Shi Hun <461770336@qq.com>
2019-02-19 15:40:06 +0800

    update welcome.blode.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by ShiHun</div>
+                <div class="title">Hello Laravel! - by ShiHunaaa</div>
             </div>
         </div>
     </body>
